5ffacee23e510ae8a836e48cac214a6688f76cc7
deancjx <deancjx@gmail.com>
2018-12-28 17:05:56 +0300

    Site için HTML dosyasını değiştirdim.

--- firstProject/urls.py
+++ firstProject/urls.py
@@ -2,15 +2,16 @@
 
 from django.contrib import admin
 from django.conf.urls import *
-from django.urls import path
+from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from ilkproje import views
 admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^about', views.AboutPageView.as_view())
+    path(r'', include('ilkproje.urls')),
+    #url(r'^$', views.HomePageView.as_view()),
+    #url(r'^about', views.AboutPageView.as_view())
 
 ]
 
--- ilkproje/views.py
+++ ilkproje/views.py
@@ -3,11 +3,13 @@ from django.http import *
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class HomePageView(TemplateView):
+"""class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, "index.html", context=None)
 
 class AboutPageView(TemplateView):
-    template_name = "about.html"
+    template_name = "about.html"""
 
+def post_list(request):
+    return render(request, 'blog/post_list.html')
 
